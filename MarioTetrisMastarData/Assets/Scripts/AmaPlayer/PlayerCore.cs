110d12cf9d698e9f8ef0f4c81fe905a16cd28cf9
AMAKASU <ziropaku@gmail.com>
2022-02-10 10:09:49 +0900

    Add　OursScene

--- MarioTetrisMastarData/Assets/Scripts/AmaPlayer/PlayerCore.cs
+++ MarioTetrisMastarData/Assets/Scripts/AmaPlayer/PlayerCore.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCpre : MonoBehaviour
+public class PlayerCore : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
