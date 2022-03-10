c0fbb0f62904692217d9679be24ac2234f4618e4
AMAKASU <ziropaku@gmail.com>
2022-03-10 23:01:45 +0900

    AddUpdate、Sprite

--- MarioTetrisMastarData/Assets/Scripts/Mario/IPlayerUpdate.cs
+++ MarioTetrisMastarData/Assets/Scripts/Mario/IPlayerUpdate.cs
@@ -1,18 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class IPlayerUpdate : MonoBehaviour
+namespace Mario
 {
-    // Start is called before the first frame update
-    void Start()
+    interface IPlayerUpdate
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public void MarioUpdate();
+        public void MarioFixedUpdate();
     }
 }
